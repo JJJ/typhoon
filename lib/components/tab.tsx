5f34d959ab93437ddb09faa64b242d90c4946b9c
John James Jacoby <johnjamesjacoby@me.com>
2024-01-21 11:33:54 -0600

    Tab: move inline styling into <style> tag

--- lib/components/tab.tsx
+++ lib/components/tab.tsx
@@ -19,13 +19,12 @@ const Tab = forwardRef<HTMLLIElement, TabProps>((props, ref) => {
     }
   };
 
-  const {isActive, isFirst, isLast, borderColor, hasActivity} = props;
+  const {isActive, isFirst, isLast, borderColor = '#ccc', hasActivity} = props;
 
   return (
     <>
       <li
         onClick={props.onClick}
-        style={{borderColor}}
         className={`tab_tab ${isFirst ? 'tab_first' : ''} ${isActive ? 'tab_active' : ''} ${
           isFirst && isActive ? 'tab_firstActive' : ''
         } ${hasActivity ? 'tab_hasActivity' : ''}`}
@@ -49,116 +48,118 @@ const Tab = forwardRef<HTMLLIElement, TabProps>((props, ref) => {
         {props.customChildren}
       </li>
 
-      <style jsx>{`
-        .tab_tab {
-          color: #ccc;
-          border-color: #ccc;
-          border-bottom-width: 1px;
-          border-bottom-style: solid;
-          border-left-width: 1px;
-          border-left-style: solid;
-          list-style-type: none;
-          flex-grow: 1;
-          position: relative;
-        }
+      <style jsx>
+        {`
+          .tab_tab {
+            color: #ccc;
+            border-color: ${borderColor};
+            border-bottom-width: 1px;
+            border-bottom-style: solid;
+            border-left-width: 1px;
+            border-left-style: solid;
+            list-style-type: none;
+            flex-grow: 1;
+            position: relative;
+          }
 
-        .tab_tab:hover {
-          color: #ccc;
-        }
+          .tab_tab:hover {
+            color: #ccc;
+          }
 
-        .tab_first {
-          border-left-width: 0;
-          padding-left: 1px;
-        }
+          .tab_first {
+            border-left-width: 0;
+            padding-left: 1px;
+          }
 
-        .tab_firstActive {
-          border-left-width: 1px;
-          padding-left: 0;
-        }
+          .tab_firstActive {
+            border-left-width: 1px;
+            padding-left: 0;
+          }
 
-        .tab_active {
-          color: #fff;
-          border-bottom-width: 0;
-        }
-        .tab_active:hover {
-          color: #fff;
-        }
+          .tab_active {
+            color: #fff;
+            border-bottom-width: 0;
+          }
+          .tab_active:hover {
+            color: #fff;
+          }
 
-        .tab_hasActivity {
-          color: #50e3c2;
-        }
+          .tab_hasActivity {
+            color: #50e3c2;
+          }
 
-        .tab_hasActivity:hover {
-          color: #50e3c2;
-        }
+          .tab_hasActivity:hover {
+            color: #50e3c2;
+          }
 
-        .tab_text {
-          transition: color 0.2s ease;
-          height: 34px;
-          display: block;
-          width: 100%;
-          position: relative;
-          overflow: hidden;
-        }
+          .tab_text {
+            transition: color 0.2s ease;
+            height: 34px;
+            display: block;
+            width: 100%;
+            position: relative;
+            overflow: hidden;
+          }
 
-        .tab_textInner {
-          position: absolute;
-          left: 24px;
-          right: 24px;
-          top: 0;
-          bottom: 0;
-          text-align: center;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-          overflow: hidden;
-        }
+          .tab_textInner {
+            position: absolute;
+            left: 24px;
+            right: 24px;
+            top: 0;
+            bottom: 0;
+            text-align: center;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            overflow: hidden;
+          }
 
-        .tab_icon {
-          transition:
-            opacity 0.2s ease,
-            color 0.2s ease,
-            transform 0.25s ease,
-            background-color 0.1s ease;
-          pointer-events: none;
-          position: absolute;
-          right: 7px;
-          top: 10px;
-          display: inline-block;
-          width: 14px;
-          height: 14px;
-          border-radius: 100%;
-          color: #e9e9e9;
-          opacity: 0;
-          transform: scale(0.95);
-        }
+          .tab_icon {
+            transition:
+              opacity 0.2s ease,
+              color 0.2s ease,
+              transform 0.25s ease,
+              background-color 0.1s ease;
+            pointer-events: none;
+            position: absolute;
+            right: 7px;
+            top: 10px;
+            display: inline-block;
+            width: 14px;
+            height: 14px;
+            border-radius: 100%;
+            color: #e9e9e9;
+            opacity: 0;
+            transform: scale(0.95);
+          }
 
-        .tab_icon:hover {
-          background-color: rgba(255, 255, 255, 0.13);
-          color: #fff;
-        }
+          .tab_icon:hover {
+            background-color: rgba(255, 255, 255, 0.13);
+            color: #fff;
+          }
 
-        .tab_icon:active {
-          background-color: rgba(255, 255, 255, 0.1);
-          color: #909090;
-        }
+          .tab_icon:active {
+            background-color: rgba(255, 255, 255, 0.1);
+            color: #909090;
+          }
 
-        .tab_tab:hover .tab_icon {
-          opacity: 1;
-          transform: none;
-          pointer-events: all;
-        }
+          .tab_tab:hover .tab_icon {
+            opacity: 1;
+            transform: none;
+            pointer-events: all;
+          }
 
-        .tab_shape {
-          position: absolute;
-          left: 4px;
-          top: 4px;
-          width: 6px;
-          height: 6px;
-          vertical-align: middle;
-          fill: currentColor;
-          shape-rendering: crispEdges;
-        }
-      `}</style>
+          .tab_shape {
+            position: absolute;
+            left: 4px;
+            top: 4px;
+            width: 6px;
+            height: 6px;
+            vertical-align: middle;
+            fill: currentColor;
+            shape-rendering: crispEdges;
+          }
+        `}
+      </style>
     </>
   );
 });
